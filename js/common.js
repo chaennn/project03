83a94bf0087c5b47fffffa5f91f143fbae9cc6cb
chaennn <girmn22@gmail.com>
2022-12-29 16:04:55 +0900

    22.12.29

--- js/common.js
+++ js/common.js
@@ -44,7 +44,7 @@ console.log(
   ###    ###  ##########  ##########  ##########   ########                                             
 `
 );
-console.log("https://github.com/chaennn");
+console.log("https://github.com/chaennn/portfolio");
 console.log(
   `%c카카오톡 ID입니다. 용건이 있으시면 연락주세요:) chaen_hhh`,
   `color:white`
